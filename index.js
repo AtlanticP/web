cd76e6670548a27957202d72830ba1f8b47ebf8f
laptop_ubuntu <davidovnv@gmail.com>
2018-09-01 22:31:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+$('div#box').on('mouseenter', function() {
+  $(this).animate({'opacity': '0.99'}, 100);
+});
+
+$('div#box').on('mouseleave', function() {
+  $(this).animate({'opacity': '0.7'}, 100);
+});
